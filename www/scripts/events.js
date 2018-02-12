7f05c828fb124c0a0ec5cd7ba36b5f6bcc4e3d55
John Redden <jtredden@gmail.com>
2018-02-11 17:12:40 -0800

    Events Order by Time
    Also added time formatting converstions from 24 hours to regular am pm
    time... function at bottom.

--- www/scripts/events.js
+++ www/scripts/events.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     //real time refresh of page is overkill but cool.
     var locationsRef = firebase.database().ref("events");
     // on -- value ... is real time
-    locationsRef.orderByChild("text").on("value", function (snapshot) {
+    locationsRef.orderByChild("startTime").on("value", function (snapshot) {
         //clear html list and remake it
         $("#eventList").html("");
 
@@ -14,7 +14,7 @@ $(document).ready(function () {
             var listHTML = "<li class='list-group-item clearfix'>" +
                 "<strong><span>" + data.val().text + "</span></strong>" +
                 "<br/><span>" + data.val().description + "</span>" +
-                "<br/><span>" + data.val().startTime + " to "+ data.val().endTime +"</span>" +
+                "<br/><span>" + formatAMPM(data.val().startTime) + " to "+ formatAMPM(data.val().endTime) +"</span>" +
                 "<span class='pull-right button-group'>" +
                 "<button id='edit" + data.key + "' type='button' class='btn btn-success btn-sm'><span class='glyphicon glyphicon-edit'></span></button>" +
                 "<button id='" + data.key + "' type='button' class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-remove'></span></button>" +
@@ -55,7 +55,17 @@ $(document).ready(function () {
 
     });
 
-
+    function formatAMPM(arg) {
+        var hours = parseInt(arg.substring(0, 2));
+        var minutes = parseInt(arg.substring(3, 5));
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+      }
+  
 
 
 });
